97dfc1af19d636edc4e1fd938b00ac776f2de186
Mason Knight <92546628+masonknight22@users.noreply.github.com>
2022-11-11 02:44:35 -0500

    Add files via upload

---
+++ ModernSketch.java
@@ -0,0 +1,678 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.pdf.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class ModernSketch extends PApplet {
+
+
+
+
+
+public void setup() {
+
+
+}
+
+
+public void draw() {
+
+noStroke();
+fill(241,158,174);
+triangle(0,0,150,150,300,0);
+
+noStroke();
+fill(241,174,185);
+triangle(0,0,30,0,30,30);
+noStroke();
+fill(241,144,162);
+triangle(30,30,60,30,60,0);
+noStroke();
+fill(241,174,185);
+triangle(60,0,60,30,90,30);
+noStroke();
+fill(241,144,162);
+triangle(90,0,90,30,120,30);
+noStroke();
+fill(241,174,185);
+triangle(90,0,120,0,120,30);
+noStroke();
+fill(241,174,185);
+triangle(120,30,150,0,150,30);
+noStroke();
+fill(241,144,162);
+triangle(150,0,150,30,180,30);
+noStroke();
+fill(241,174,185);
+triangle(180,30,180,0,210,0);
+noStroke();
+fill(241,144,162);
+triangle(210,0,210,30,240,30);
+noStroke();
+fill(241,174,185);
+triangle(210,0,240,0,240,30);
+noStroke();
+fill(241,144,162);
+triangle(240,0,270,30,270,0);
+
+noStroke();
+fill(241,174,185);
+triangle(270,30,240,30,240,60);
+noStroke();
+fill(241,144,162);
+triangle(210,30,210,60,240,60);
+noStroke();
+fill(241,174,185);
+triangle(180,30,210,30,210,60);
+noStroke();
+fill(241,174,185);
+triangle(180,30,180,60,150,60);
+noStroke();
+fill(241,144,162);
+triangle(120,30,150,30,150,60);
+noStroke();
+fill(241,174,185);
+triangle(120,30,90,30,90,60);
+noStroke();
+fill(241,144,162);
+triangle(60,30,90,30,90,60);
+noStroke();
+fill(241,174,185);
+triangle(30,30,60,30,60,60);
+
+noStroke();
+fill(241,174,185);
+triangle(60,60,90,60,90,90);
+noStroke();
+fill(241,144,162);
+triangle(90,60,120,60,90,90);
+noStroke();
+fill(241,174,185);
+triangle(120,60,150,60,150,90);
+noStroke();
+fill(241,144,162);
+triangle(120,60,120,90,150,90);
+noStroke();
+fill(241,144,162);
+triangle(180,60,180,90,150,90);
+noStroke();
+fill(241,174,185);
+triangle(180,60,210,60,210,90);
+noStroke();
+fill(241,174,185);
+triangle(90,90,120,90,120,120);
+noStroke();
+fill(241,174,185);
+triangle(210,90,180,90,180,120);
+
+
+noStroke();
+fill(240,205,70);
+triangle(0,300,300,300,150,150);
+
+noStroke();
+fill(240,220,120);
+ellipse(15,285,30,30);
+noStroke();
+fill(245,210,80);
+ellipse(45,285,30,30);
+noStroke();
+fill(240,220,120);
+ellipse(75,285,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(105,285,30,30);
+noStroke();
+fill(245,210,80);
+ellipse(135,285,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(165,285,30,30);
+noStroke();
+fill(240,220,120);
+ellipse(195,285,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(225,285,30,30);
+noStroke();
+fill(245,210,80);
+ellipse(255,285,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(285,285,30,30);
+
+noStroke();
+fill(240,220,120);
+ellipse(45,255,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(75,255,30,30);
+noStroke();
+fill(245,210,80);
+ellipse(105,255,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(135,255,30,30);
+noStroke();
+fill(240,220,120);
+ellipse(165,255,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(195,255,30,30);
+noStroke();
+fill(245,210,80);
+ellipse(225,255,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(255,255,30,30);
+
+noStroke();
+fill(245,210,80);
+ellipse(75,225,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(105,225,30,30);
+noStroke();
+fill(245,210,80);
+ellipse(135,225,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(165,225,30,30);
+noStroke();
+fill(240,220,120);
+ellipse(195,225,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(225,225,30,30);
+
+noStroke();
+fill(240,220,120);
+ellipse(105,195,30,30);
+noStroke();
+fill(244,200,50);
+ellipse(195,195,30,30);
+
+
+
+noStroke();
+fill(235,235,235);
+triangle(300,0,300,300,150,150);
+
+noStroke();
+fill(69,60,95);
+quad(300,0,295,5,295,295,300,300);
+
+quad(290,10,285,15,285,285,290,290);
+
+quad(280,20,275,25,275,275,280,280);
+
+quad(270,30,265,35,265,265,270,270);
+
+quad(260,40,255,45,255,255,260,260);
+
+quad(250,50,245,55,245,245,250,250);
+
+quad(240,60,235,65,235,235,240,240);
+
+quad(230,70,225,75,225,225,230,230);
+
+quad(220,80,215,85,215,215,220,220);
+
+quad(210,90,205,95,205,205,210,210);
+
+quad(200,100,195,105,195,195,200,200);
+
+quad(190,110,185,115,185,185,190,190);
+
+
+noStroke();
+fill(235,235,235);
+triangle(0,0,150,150,0,300);
+
+noStroke();
+fill(0,0,0);
+ellipse(5,15,5,5);
+ellipse(14,24,5,5);
+ellipse(23,34,5,5);
+ellipse(32,42,5,5);
+ellipse(41,51,5,5);
+ellipse(50,60,5,5);
+ellipse(59,69,5,5);
+ellipse(68,78,5,5);
+ellipse(77,87,5,5);
+ellipse(86,96,5,5);
+ellipse(95,105,5,5);
+ellipse(104,114,5,5);
+ellipse(5,29,5,5);
+ellipse(5,40,5,5);
+ellipse(5,51,5,5);
+ellipse(5,62,5,5);
+ellipse(5,73,5,5);
+ellipse(5,84,5,5);
+ellipse(5,95,5,5);
+ellipse(5,106,5,5);
+ellipse(5,117,5,5);
+ellipse(5,128,5,5);
+ellipse(5,139,5,5);
+ellipse(5,150,5,5);
+ellipse(5,161,5,5);
+ellipse(5,172,5,5);
+ellipse(5,183,5,5);
+ellipse(5,194,5,5);
+ellipse(5,205,5,5);
+ellipse(5,216,5,5);
+ellipse(5,227,5,5);
+ellipse(5,238,5,5);
+ellipse(5,249,5,5);
+ellipse(5,260,5,5);
+ellipse(5,271,5,5);
+ellipse(5,282,5,5);
+ellipse(15,273,5,5);
+ellipse(24,264,5,5);
+ellipse(33,255,5,5);
+ellipse(42,246,5,5);
+ellipse(51,237,5,5);
+ellipse(60,228,5,5);
+ellipse(69,219,5,5);
+ellipse(78,210,5,5);
+ellipse(87,201,5,5);
+ellipse(96,192,5,5);
+ellipse(105,183,5,5);
+ellipse(114,174,5,5);
+ellipse(123,165,5,5);
+ellipse(132,156,5,5);
+
+ellipse(14,37,5,5);
+ellipse(14,48,5,5);
+ellipse(14,59,5,5);
+ellipse(14,70,5,5);
+ellipse(14,81,5,5);
+ellipse(14,92,5,5);
+ellipse(15,103,5,5);
+ellipse(15,114,5,5);
+ellipse(15,125,5,5);
+ellipse(15,136,5,5);
+ellipse(15,147,5,5);
+ellipse(15,158,5,5);
+ellipse(15,169,5,5);
+ellipse(15,180,5,5);
+ellipse(15,191,5,5);
+ellipse(15,202,5,5);
+ellipse(14,213,5,5);
+ellipse(14,224,5,5);
+ellipse(14,235,5,5);
+ellipse(14,246,5,5);
+ellipse(14,259,5,5);
+ellipse(23,46,5,5);
+ellipse(32,56,5,5);
+ellipse(41,65,5,5);
+ellipse(50,74,5,5);
+ellipse(59,83,5,5);
+ellipse(68,92,5,5);
+ellipse(77,101,5,5);
+ellipse(86,110,5,5);
+ellipse(95,119,5,5);
+ellipse(104,128,5,5);
+ellipse(113,137,5,5);
+ellipse(22,252,5,5);
+ellipse(31,243,5,5);
+ellipse(40,234,5,5);
+ellipse(49,225,5,5);
+ellipse(58,216,5,5);
+ellipse(67,207,5,5);
+ellipse(76,198,5,5);
+ellipse(85,189,5,5);
+ellipse(94,180,5,5);
+ellipse(103,171,5,5);
+ellipse(25,63,5,5);
+ellipse(24,73,5,5);
+ellipse(24,84,5,5);
+ellipse(24,95,5,5);
+ellipse(24,106,5,5);
+ellipse(24,117,5,5);
+ellipse(24,128,5,5);
+ellipse(25,139,5,5);
+ellipse(25,150,5,5);
+ellipse(25,161,5,5);
+ellipse(26,172,5,5);
+ellipse(25,184,5,5);
+ellipse(24,196,5,5);
+ellipse(24,208,5,5);
+ellipse(24,220,5,5);
+ellipse(24,234,5,5);
+ellipse(36,74,5,5);
+ellipse(45,83,5,5);
+ellipse(54,92,5,5);
+ellipse(63,101,5,5);
+ellipse(72,110,5,5);
+ellipse(81,119,5,5);
+ellipse(90,128,5,5);
+ellipse(99,137,5,5);
+ellipse(35,225,5,5);
+ellipse(44,216,5,5);
+ellipse(54,206,5,5);
+ellipse(63,197,5,5);
+ellipse(72,188,5,5);
+ellipse(81,179,5,5);
+ellipse(90,170,5,5);
+ellipse(99,161,5,5);
+ellipse(108,152,5,5);
+ellipse(34,87.5f,5,5);
+ellipse(34,99,5,5);
+ellipse(34,111,5,5);
+ellipse(34,123,5,5);
+ellipse(35,134,5,5);
+ellipse(35,146,5,5);
+ellipse(35,157,5,5);
+ellipse(37,166.5f,5,5);
+ellipse(35,179,5,5);
+ellipse(34,190,5,5);
+ellipse(34,201,5,5);
+ellipse(34,212,5,5);
+ellipse(43,203,5,5);
+ellipse(52,194,5,5);
+ellipse(61,185,5,5);
+ellipse(70,176,5,5);
+ellipse(79,167,5,5);
+ellipse(88,158,5,5);
+ellipse(97,149,5,5);
+ellipse(43,95,5,5);
+ellipse(52,103,5,5);
+ellipse(61,112,5,5);
+ellipse(70,121,5,5);
+ellipse(79,130,5,5);
+ellipse(88,139,5,5);
+ellipse(43,108,5,5);
+ellipse(44,119,5,5);
+ellipse(45,130,5,5);
+ellipse(45,141,5,5);
+ellipse(45,153,5,5);
+ellipse(49,164,5,5);
+ellipse(44,175,5,5);
+ellipse(44,188,5,5);
+ellipse(53,179,5,5);
+ellipse(62,170,5,5);
+ellipse(72,160,5,5);
+ellipse(81,149,5,5);
+ellipse(53,119,5,5);
+ellipse(62,127,5,5);
+ellipse(73,138,5,5);
+ellipse(54,134,5,5);
+ellipse(62,139,5,5);
+ellipse(70,149,5,5);
+ellipse(61.5f,159,5,5);
+ellipse(56,150,5,5);
+
+
+noStroke();
+fill(255,255,255);
+
+
+beginShape();
+  vertex(30,110);
+  vertex(110,30);
+  bezierVertex(120,20,130,30,120,40);
+  vertex(110,50);
+  bezierVertex(100,60,110,70,120,60);
+  vertex(160,20);
+  bezierVertex(170,10,180,20,170,30);
+  vertex(140,60);
+  bezierVertex(130,70,140,80,150,70);
+  vertex(160,60);
+  bezierVertex(170,50,180,60,170,70);
+  bezierVertex(160,80,170,90,180,80);
+  vertex(220,40);
+  bezierVertex(230,30,240,40,230,50);
+  vertex(200,80);
+  bezierVertex(190,90,200,100,210,90);
+  vertex(220,80);
+  bezierVertex(230,70,240,80,230,90);
+  vertex(210,110);
+  bezierVertex(200,120,210,130,220,120);
+  vertex(230,110);
+  bezierVertex(240,100,250,110,240,120);
+  vertex(230,130);
+  bezierVertex(220,140,230,150,240,140);
+  vertex(270,110);
+  bezierVertex(280,100,290,110,280,120);
+  
+  vertex(230,170);
+  bezierVertex(220,180,230,190,240,180);
+  vertex(260,160);
+  bezierVertex(270,150,280,160,270,170);
+  vertex(180,260);
+  bezierVertex(170,270,160,260,170,250);
+  vertex(190,230);
+  bezierVertex(200,220,190,210,180,220);
+  vertex(160,240);
+  bezierVertex(150,250,140,240,150,230);
+  vertex(150,230);
+  bezierVertex(160,220,150,210,140,220);
+  vertex(110,250);
+  bezierVertex(100,260,90,250,100,240);
+  vertex(110,230);
+  bezierVertex(120,220,110,210,100,220);
+  vertex(60,260);
+  bezierVertex(50,270,40,260,50,250);
+  vertex(80,220);
+  bezierVertex(90,210,80,200,70,210);
+  vertex(60,220);
+  bezierVertex(50,230,40,220,50,210);
+  vertex(80,180);
+  bezierVertex(90,170,80,160,70,170);
+  vertex(60,180);
+  bezierVertex(50,190,40,180,50,170);
+  vertex(80,140);
+  bezierVertex(90,130,80,120,70,130);
+  vertex(60,140);
+  bezierVertex(50,150,40,140,50,130);
+  vertex(70,110);
+  bezierVertex(80,100,70,90,60,100);
+  vertex(40,120);
+  bezierVertex(30,130,20,120,30,110);
+  
+  
+  
+ endShape();
+
+
+
+ fill(240,205,70);
+ beginShape();
+  vertex(150,150);
+  vertex(107.5736f,192.42641f);//add 42.4264   bezier is 22.96101
+  bezierVertex(131,215.85787f,168.99595f,215.85787f,192.42641f,192.42641f);   //23.431458
+  vertex(150,150);
+ endShape();
+
+fill(240,220,120);
+ellipse(135,165,30,30);
+fill(244,200,50);
+ellipse(165,165,30,30);
+
+fill(240,220,120);
+ellipse(105,195,30,30);
+fill(244,200,50);
+ellipse(135,195,30,30);
+fill(245,210,80);
+ellipse(165,195,30,30);
+fill(244,200,50);
+ellipse(195,195,30,30);
+
+
+
+noStroke();
+fill(235,235,235);
+
+
+beginShape();
+  vertex(150,150);
+  vertex(192.42641f,107.57359f);//add 42.4264   bezier is 22.96101
+  bezierVertex(215.85787f,131,215.85787f,168.99595f,192.42641f,192.42641f);   //23.431458
+  vertex(150,150);
+ endShape();
+
+noStroke();
+fill(69,60,95);
+quad(155,155,160,160,160,140,155,145);
+
+quad(165,165,170,170,170,130,165,135);
+
+quad(175,175,180,180,180,120,175,125);
+
+quad(185,185,190,190,190,110,185,115);
+
+quad(195,200,200,190,200,100,195,105);
+
+quad(205,123,210,129,210,180,205,180);
+noStroke();
+fill(235,235,235);
+beginShape();
+  vertex(150,150);
+  vertex(107.5736f,107.5736f);//add 42.4264   bezier is 22.96101
+  bezierVertex(84.14214f,131,84.14214f,168.99595f,107.5736f,192.42641f);   //23.431458
+  vertex(150,150);
+ endShape();
+ 
+ 
+ 
+ 
+ 
+ 
+noStroke();
+fill(241,158,174);
+ 
+ beginShape();
+  vertex(150,150);
+  vertex(107.5736f,107.5736f);//add 42.4264   bezier is 22.96101
+  bezierVertex(131,84.14214f,168.99595f,84.14214f,192.42641f,107.5736f);   //23.431458
+  vertex(150,150);
+ endShape();
+ 
+ fill(241,158,174);
+ triangle(150,150,120,120,150,120);
+ fill(241,144,162);
+ triangle(150,150,180,120,150,120);
+ 
+ fill(241,144,162);
+ triangle(120,120,150,90,150,120);
+ fill(241,174,185);
+ triangle(150,90,180,120,150,120);
+ 
+ fill(241,144,162);
+ triangle(180,90,180,120,150,90);
+ fill(241,174,185);
+ triangle(120,120,105,105,120,95);
+   //vertex(180,90.038);
+   
+ fill(0,0,0);
+ ellipse(138,151,5,5);
+ ellipse(129,142,5,5);
+ ellipse(120,133,5,5);
+ ellipse(111,124,5,5);
+ ellipse(102,115,5,5);
+
+ ellipse(129,160,5,5);
+ ellipse(120,169,5,5);
+ ellipse(111,178,5,5);
+ ellipse(102,187,5,5);
+
+ ellipse(102,176,5,5);
+ ellipse(100,165,5,5);
+ ellipse(102,154,5,5);
+ ellipse(97,145,5,5);
+ ellipse(102,132,5,5);
+ ellipse(96,123,5,5);
+ ellipse(90,158,5,5);
+ ellipse(123,150,5,5);
+ ellipse(110,164,5,5);
+ ellipse(110,140,5,5);
+ ellipse(113,154,5,5);
+
+
+
+
+
+fill(255,255,255);
+beginShape();
+  vertex(107.5736f,107.5736f);
+  bezierVertex(131,84.14214f,168.99595f,84.14214f,192.42641f,107.5736f);
+  vertex(185,80);
+  vertex(160,88);
+  vertex(150,88);
+  vertex(115,80);
+  vertex(105,106);
+  
+ endShape();
+
+beginShape();
+  vertex(107.5736f,107.5736f);//add 42.4264   bezier is 22.96101
+  bezierVertex(84.14214f,131,84.14214f,168.99595f,107.5736f,192.42641f);
+  vertex(90,210);
+  vertex(90,180);
+  vertex(80,150);
+  vertex(98,110);
+  
+ endShape();
+
+
+
+beginShape();
+  vertex(107.5736f,192.42641f);//add 42.4264   bezier is 22.96101
+  bezierVertex(131,215.85787f,168.99595f,215.85787f,192.42641f,192.42641f); 
+  vertex(210,210);
+  vertex(90,210);
+  vertex(90,180);
+  
+ endShape();
+ 
+
+
+beginShape();
+  vertex(192.42641f,107.57359f);//add 42.4264   bezier is 22.96101
+  bezierVertex(215.85787f,131,215.85787f,168.99595f,192.42641f,192.42641f); 
+  vertex(210,210);
+  vertex(210,125);
+  vertex(195,88);
+
+ endShape();
+ 
+
+beginShape();
+  
+  vertex(215.85787f,168.99595f);
+  vertex(168.99595f,215.85787f);
+  vertex(200,210);
+  vertex(219,219);
+
+endShape();
+
+
+
+ 
+ 
+ 
+ 
+}
+  public void settings() { 
+size(300,300,PDF, "knight-mason-pz.pdf"); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "ModernSketch" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
